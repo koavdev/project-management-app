21ba363b1f2d320a11f9f9f03070d266523d4f7e
koavdev <koavdev@gmail.com>
2023-05-04 20:18:54 -0300

    c2 - create the connection to database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
+const colors = require('colors');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema,
